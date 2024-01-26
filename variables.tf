c7136aaf7444c1fc24bc067ed219b6654ef1a820
kz.li <kz.li@hashicorp.com>
2024-01-26 16:56:10 +0800

    Networking baseline

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "vpc_name" {
+  type        = string
+  description = "VPC Name"
+  default     = "likz-vpc"
+}
+
+variable "vpc_cidr_block" {
+  type        = string
+  default     = "10.0.0.0/16"
+  description = "CIDR Range for VPC"
+}
+
+variable "public_subnet_cidrs" {
+  type        = list(string)
+  description = "Public Subnet CIDR values"
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  type        = list(string)
+  description = "Private Subnet CIDR values"
+  default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}
+
+variable "availability_zones" {
+ type        = list(string)
+ description = "Availability Zones"
+ default     = ["ap-southeast-1a", "ap-southeast-1b", "ap-southeast-1c"]
+}
